56e216a8702f1ce1a68fa0bedda758833a59d964
FaultyRAM <316014+FaultyRAM@users.noreply.github.com>
2021-03-26 20:22:14 +1000

    feat: add `generate_response` stub

--- src/lib.rs
+++ src/lib.rs
@@ -26,3 +26,27 @@ use std as std_crate;
 mod error;
 
 pub use error::{Error, InvalidChallenge, InvalidKey};
+
+/// The generated response to a secure/validate challenge.
+#[derive(Clone, Copy, Debug)]
+pub struct Output {
+    buffer: [u8; 88],
+    len: usize,
+}
+
+/// Generates a secure/validate response from a secret key and challenge.
+///
+/// # Errors
+///
+/// This function returns an `Error` if the secret key or challenge are invalid:
+///
+/// * `Error::InvalidKey` if the secret key is invalid; the inner `InvalidKey` specifies the exact
+///   problem
+/// * `Error::InvalidChallenge` if the challenge is invalid; the inner `InvalidChallenge` specifies
+///   the exact problem
+pub const fn generate_response(_secret_key: &[u8], _challenge: &[u8]) -> Result<Output, Error> {
+    Ok(Output {
+        buffer: [0; 88],
+        len: 0,
+    })
+}
