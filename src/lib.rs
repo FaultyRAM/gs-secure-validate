18fdadd43edd91befa1239e6232166b86fbbb37a
FaultyRAM <316014+FaultyRAM@users.noreply.github.com>
2021-03-27 17:48:11 +1000

    refactor: more code cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@
     rustdoc,
     unused
 )]
-#![allow(clippy::clippy::must_use_candidate)]
+#![allow(clippy::must_use_candidate)]
 
 #[cfg(not(feature = "std"))]
 use core as std_crate;
@@ -33,16 +33,21 @@ use std_crate::{
     str,
 };
 
+macro_rules! swap {
+    ($a:expr, $b:expr) => {
+        #[allow(clippy::manual_swap)]
+        {
+            let t = $a;
+            $a = $b;
+            $b = t;
+        }
+    };
+}
+
 /// The generated response to a secure/validate challenge.
 #[derive(Clone, Copy, Debug)]
 pub struct Output {
     buffer: [u8; 88],
-    len: usize,
-}
-
-#[derive(Clone, Copy, Debug)]
-struct State {
-    buffer: [u8; 66],
     len: usize,
 }
 
@@ -87,34 +92,100 @@ impl Output {
     /// * `Error::InvalidChallenge` if the challenge is invalid; the inner `ChallengeError`
     ///   specifies the exact problem
     pub const fn generate(secret_key: &[u8], challenge: &[u8]) -> Result<Self, Error> {
-        match State::generate(secret_key, challenge) {
-            Ok(state) => {
-                let num_src_buffers = (state.len / 3) + ((state.len % 3 > 0) as usize);
-                let mut output = Self {
-                    buffer: [0; 88],
-                    len: num_src_buffers * 4,
-                };
-                let mut i = 0;
-                while i < num_src_buffers {
-                    let a = i * 3;
-                    let b = i * 4;
-                    let encoded =
-                        encode([state.buffer[a], state.buffer[a + 1], state.buffer[a + 2]]);
-                    output.buffer[b] = encoded[0];
-                    output.buffer[b + 1] = encoded[1];
-                    output.buffer[b + 2] = encoded[2];
-                    output.buffer[b + 3] = encoded[3];
-                    i += 1;
+        #[inline]
+        const fn has_interior_nul(src: &[u8]) -> bool {
+            let mut i = 0;
+            while i < src.len() {
+                if src[i] == 0 {
+                    return true;
                 }
-                Ok(output)
+                i += 1;
             }
-            Err(e) => Err(e),
+            false
+        }
+
+        if secret_key.is_empty() {
+            Err(Error::InvalidKey(KeyError::ZeroLength))
+        } else if secret_key.len() > 256 {
+            Err(Error::InvalidKey(KeyError::TooLong))
+        } else if has_interior_nul(secret_key) {
+            Err(Error::InvalidKey(KeyError::InteriorNul))
+        } else if challenge.len() > 64 {
+            Err(Error::InvalidChallenge(ChallengeError::TooLong))
+        } else if has_interior_nul(challenge) {
+            Err(Error::InvalidChallenge(ChallengeError::InteriorNul))
+        } else {
+            let mut tmp = [
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+                23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
+                44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
+                65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85,
+                86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104,
+                105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120,
+                121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136,
+                137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152,
+                153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168,
+                169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184,
+                185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200,
+                201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216,
+                217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232,
+                233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248,
+                249, 250, 251, 252, 253, 254, 255,
+            ];
+            let mut loop_index = 0;
+            let mut x = 0;
+            while loop_index < tmp.len() {
+                x = secret_key[loop_index % secret_key.len()]
+                    .wrapping_add(tmp[loop_index])
+                    .wrapping_add(x);
+                swap!(tmp[loop_index], tmp[x as usize]);
+                loop_index += 1;
+            }
+            loop_index = 0;
+            x = 0;
+            let mut y = 0;
+            let mut state_buffer = [0; 66];
+            while loop_index < challenge.len() {
+                x = challenge[loop_index].wrapping_add(x).wrapping_add(1);
+                let xu = x as usize;
+                y = tmp[xu].wrapping_add(y);
+                let yu = y as usize;
+                swap!(tmp[xu], tmp[yu]);
+                let j = (tmp[xu].wrapping_add(tmp[yu])) as usize;
+                state_buffer[loop_index] = challenge[loop_index] ^ tmp[j];
+                loop_index += 1;
+            }
+            let num_src_buffers = (challenge.len() / 3) + ((challenge.len() % 3 > 0) as usize);
+            let mut output = Self {
+                buffer: [0; 88],
+                len: num_src_buffers * 4,
+            };
+            loop_index = 0;
+            while loop_index < num_src_buffers {
+                const ENCODE_TABLE: [u8; 64] = [
+                    b'A', b'B', b'C', b'D', b'E', b'F', b'G', b'H', b'I', b'J', b'K', b'L', b'M',
+                    b'N', b'O', b'P', b'Q', b'R', b'S', b'T', b'U', b'V', b'W', b'X', b'Y', b'Z',
+                    b'a', b'b', b'c', b'd', b'e', b'f', b'g', b'h', b'i', b'j', b'k', b'l', b'm',
+                    b'n', b'o', b'p', b'q', b'r', b's', b't', b'u', b'v', b'w', b'x', b'y', b'z',
+                    b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'+', b'/',
+                ];
+                let a = loop_index * 3;
+                let b = loop_index * 4;
+                output.buffer[b] = ENCODE_TABLE[(state_buffer[a] >> 2) as usize];
+                output.buffer[b + 1] = ENCODE_TABLE
+                    [(((state_buffer[a] & 0b11) << 4) | (state_buffer[a + 1] >> 4)) as usize];
+                output.buffer[b + 2] = ENCODE_TABLE
+                    [(((state_buffer[a + 1] & 0b1111) << 2) | (state_buffer[a + 2] >> 6)) as usize];
+                output.buffer[b + 3] = ENCODE_TABLE[(state_buffer[a + 2] & 0b11_1111) as usize];
+                loop_index += 1;
+            }
+            Ok(output)
         }
     }
 
     /// Returns the generated response as a string slice.
     pub fn as_str(&self) -> &str {
-        // SAFETY: `encode` guarantees that this will be a valid ASCII string.
+        // SAFETY: guaranteed to be a valid ASCII string. See `ENCODE_TABLE` above.
         unsafe { str::from_utf8_unchecked(self.as_bytes()) }
     }
 
@@ -160,115 +231,6 @@ impl Display for Output {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         f.write_str(self.as_str())
     }
-}
-
-impl State {
-    #[allow(clippy::clippy::manual_swap)] // False positive; `mem::swap` is not `const`.
-    const fn generate(key: &[u8], challenge: &[u8]) -> Result<Self, Error> {
-        if let Err(e) = check_key(key) {
-            Err(Error::InvalidKey(e))
-        } else if let Err(e) = check_challenge(challenge) {
-            Err(Error::InvalidChallenge(e))
-        } else {
-            let mut tmp = [
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-                23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
-                44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
-                65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85,
-                86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104,
-                105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120,
-                121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136,
-                137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152,
-                153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168,
-                169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184,
-                185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200,
-                201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216,
-                217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232,
-                233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248,
-                249, 250, 251, 252, 253, 254, 255,
-            ];
-            let mut loop_index = 0;
-            let mut key_index = 0;
-            let mut x = 0;
-            while loop_index < tmp.len() {
-                x = key[key_index].wrapping_add(tmp[loop_index]).wrapping_add(x);
-                let t = tmp[loop_index];
-                tmp[loop_index] = tmp[x as usize];
-                tmp[x as usize] = t;
-                loop_index += 1;
-                key_index = (key_index + 1) % key.len();
-            }
-            loop_index = 0;
-            x = 0;
-            let mut y = 0;
-            let mut state = Self {
-                buffer: [0; 66],
-                len: challenge.len(),
-            };
-            while loop_index < challenge.len() {
-                x = challenge[loop_index].wrapping_add(x).wrapping_add(1);
-                let xu = x as usize;
-                y = tmp[xu].wrapping_add(y);
-                let yu = y as usize;
-                let t = tmp[xu];
-                tmp[xu] = tmp[yu];
-                tmp[yu] = t;
-                let j = (tmp[xu].wrapping_add(tmp[yu])) as usize;
-                state.buffer[loop_index] = challenge[loop_index] ^ tmp[j];
-                loop_index += 1;
-            }
-            Ok(state)
-        }
-    }
-}
-
-const fn check_key(key: &[u8]) -> Result<(), KeyError> {
-    if key.is_empty() {
-        Err(KeyError::ZeroLength)
-    } else if key.len() > 256 {
-        Err(KeyError::TooLong)
-    } else if has_interior_nul(key) {
-        Err(KeyError::InteriorNul)
-    } else {
-        Ok(())
-    }
-}
-
-const fn check_challenge(challenge: &[u8]) -> Result<(), ChallengeError> {
-    if challenge.len() > 64 {
-        Err(ChallengeError::TooLong)
-    } else if has_interior_nul(challenge) {
-        Err(ChallengeError::InteriorNul)
-    } else {
-        Ok(())
-    }
-}
-
-const fn has_interior_nul(src: &[u8]) -> bool {
-    let mut i = 0;
-    while i < src.len() {
-        if src[i] == 0 {
-            return true;
-        }
-        i += 1;
-    }
-    false
-}
-
-const fn encode(src: [u8; 3]) -> [u8; 4] {
-    const ENCODE_TABLE: [u8; 64] = [
-        b'A', b'B', b'C', b'D', b'E', b'F', b'G', b'H', b'I', b'J', b'K', b'L', b'M', b'N', b'O',
-        b'P', b'Q', b'R', b'S', b'T', b'U', b'V', b'W', b'X', b'Y', b'Z', b'a', b'b', b'c', b'd',
-        b'e', b'f', b'g', b'h', b'i', b'j', b'k', b'l', b'm', b'n', b'o', b'p', b'q', b'r', b's',
-        b't', b'u', b'v', b'w', b'x', b'y', b'z', b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7',
-        b'8', b'9', b'+', b'/',
-    ];
-    [
-        ENCODE_TABLE[(src[0] >> 2) as usize],
-        ENCODE_TABLE[(((src[0] & 0b11) << 4) | (src[1] >> 4)) as usize],
-        ENCODE_TABLE[(((src[1] & 0b1111) << 2) | (src[2] >> 6)) as usize],
-        ENCODE_TABLE[(src[2] & 0b11_1111) as usize],
-    ]
 }
 
 impl From<KeyError> for Error {
